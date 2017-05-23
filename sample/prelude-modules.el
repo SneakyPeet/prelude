30e525152f2555242387df74f102058c667612c1
Pieter Koornhof <koornhof.pieter@gmail.com>
2017-05-23 15:05:11 +0200

    Select prelude modules

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -11,11 +11,11 @@
 ;; (require 'prelude-evil)
 
 ;;; Programming languages support
-(require 'prelude-c)
-;; (require 'prelude-clojure)
+;; (require 'prelude-c)
+(require 'prelude-clojure)
 ;; (require 'prelude-coffee)
 ;; (require 'prelude-common-lisp)
-;; (require 'prelude-css)
+(require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
@@ -26,11 +26,11 @@
 (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-perl)
+;; (require 'prelude-perl)
 ;; (require 'prelude-python)
 ;; (require 'prelude-ruby)
 ;; (require 'prelude-scala)
-(require 'prelude-scheme)
+;; (require 'prelude-scheme)
 (require 'prelude-shell)
 ;; (require 'prelude-scss)
 ;; (require 'prelude-web) ;; Emacs mode for web templates
