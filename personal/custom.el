dccbbf5d217cbabd7bd20ca1b89fad933854cc2c
Pieter Koornhof <koornhof.pieter@gmail.com>
2017-05-24 11:15:16 +0200

    Setup eslint in js2-mode

--- personal/custom.el
+++ personal/custom.el
@@ -11,6 +11,29 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+)
 
+;; ***************************************************************************
+;; VARIABLES
+(setq js2-basic-offset 2)
+
+;; ***************************************************************************
+;; STARTUP
 (toggle-frame-maximized)
+
+;; ***************************************************************************
+;; JAVASCRIPT
+;; JS2-mode
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
+(js2-mode-hide-warnings-and-errors)
