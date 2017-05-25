accc364a52b589868cbf355dc31234bf0c9d54a8
Pieter Koornhof <koornhof.pieter@gmail.com>
2017-05-25 15:05:22 +0200

    Add nyan-mode because awesome

--- personal/custom.el
+++ personal/custom.el
@@ -5,13 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (clojure-mode vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
+    (nyan-mode yaml-mode clojure-mode vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-)
+ )
 
 ;; ***************************************************************************
 ;; VARIABLES
@@ -37,3 +37,9 @@
 (add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
 
 (js2-mode-hide-warnings-and-errors)
+
+;; ***************************************************************************
+;; Nyan
+
+(prelude-require-packages '(nyan-mode))
+(nyan-mode)
